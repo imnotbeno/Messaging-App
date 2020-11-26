6d4327b51664020a018ef4ff15c26b43c85738a0
Niko Bec <nikobec96@gmail.com>
2020-11-26 18:31:07 +0100

    added navigation bar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Bottom from "./components/Bottom";
 import MsgWindow from "./components/MessageWindow";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <MsgWindow />
       <Bottom />
     </div>
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+function NavBar() {
+  return (
+    <Drawer variant="permanent" anchor="left">
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+}
+
+export default NavBar;
