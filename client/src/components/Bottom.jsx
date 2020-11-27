2fe3203ecd2d828ea34fc582303f5ebe560c0c39
Niko Bec <nikobec96@gmail.com>
2020-11-27 18:10:36 +0100

    added button to bottom bar

--- client/src/components/Bottom.jsx
+++ client/src/components/Bottom.jsx
@@ -1,15 +1,38 @@
 import React from "react";
-import { TextField } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from "@material-ui/core/Button";
+import SendIcon from '@material-ui/icons/Send';
+
+const useStyles = makeStyles((theme) => ({
+  bottom: {
+    display: "flex",
+  },
+  button: {
+    boxShadow: "none",
+  },
+}));
 
 function Bottom() {
+
+  const classes = useStyles();
+
   return (
-    <footer>
+    <footer className={classes.bottom}>
       <TextField
         id="outlined-basic"
         label="Input Message"
         variant="outlined"
         fullWidth={true}
       />
+      <Button
+        variant="contained"
+        color="default"
+        className={classes.button}
+        endIcon={<SendIcon />}
+      >
+        Send
+      </Button>
     </footer>
   );
 }
