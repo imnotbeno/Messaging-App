e8be26cd3cd95c982f122e1f4d4d8ee5d6c350e7
Niko Bec <nikobec96@gmail.com>
2020-11-27 10:44:40 +0100

    added avatars to nav bard

--- client/src/components/MessageWindow.jsx
+++ client/src/components/MessageWindow.jsx
@@ -3,14 +3,7 @@ import Box from "@material-ui/core/Box";
 
 function MsgWindow() {
   return (
-    <Box>
-      <Box className="msg-window" display="flex" border={1}>
-        <h1>HELLO!</h1>
-      </Box>
-      <Box className="msg-window2" display="flex" flexDirection="row-reverse" border={1}>
-        <h1>HI!</h1>
-      </Box>
-    </Box>
+    <Box></Box>
   );
 }
 
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,6 +3,9 @@ import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
 
 function NavBar() {
   return (
@@ -11,6 +14,16 @@ function NavBar() {
         {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
           <ListItem button key={text}>
             <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["NB", "LR", "TK"].map((text, index) => (
+          <ListItem className="user-avatar" display="flex">
+            <IconButton>
+              <Avatar>{text}</Avatar>
+            </IconButton>
           </ListItem>
         ))}
       </List>
