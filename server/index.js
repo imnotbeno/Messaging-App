81a0194112224df05cdea6184e74663f17d64739
Niko Bec <nikobec96@gmail.com>
2020-11-30 17:52:31 +0100

    backend logic to create and save new message to db

--- server/index.js
+++ server/index.js
@@ -2,12 +2,53 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const path = require("path");
+const mongoose = require("mongoose");
+const Message = require("./message");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+mongoose.connect("mongodb://localhost:27017/messagesDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-io.on("connection", (socket) => {});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+io.on("connection", (socket) => {
+  //Get the last 10 messages from database
+  Message.find()
+    .sort({ createdAt: -1 })
+    .limit(10)
+    .exec((err, messages) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // Send last 10 messages from database to user.
+        socket.emit("init", messages);
+      }
+    });
+
+  // Listening to connected users for new messages
+  socket.on("newMessage", (msg) => {
+    // Create new message and store it in the db
+    const message = new Message({
+      user: msg.user,
+      content: msg.content,
+    });
+
+    message.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Message successfully saved");
+      }
+    });
+    //Notify all users about new message
+    socket.broadcast.emit("push", msg);
+    
+  });
+});
 
 http.listen(3000, () => {
   console.log("Server running on port 3000");
